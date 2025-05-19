d3efb700374f4604cd1371c615527e490e0ba71c
jackual <162829405+jackual@users.noreply.github.com>
2025-05-19 22:05:24 +0100

    fixed frame

--- mandu.js
+++ mandu.js
@@ -1,11 +1,11 @@
 function init() {
     const dumpling = document.querySelector("img"),
         picture = new URLSearchParams(window.location.search).get("i") || "mandu"
-    let direction = 1
+    let direction = 1, frame = 1
     dumpling.src = `img/${picture}/1.png`
     dumpling.addEventListener("click", () => {
         frame += direction
-
+        console.log(frame)
         if (frame >= 3) direction = -1
         else if (frame <= 1) direction = 1
 
